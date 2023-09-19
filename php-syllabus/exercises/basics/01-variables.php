0b9b918b7b20f925bee3e9f4259ce638467e4937
xanthopsia <80963894+xanthopsia@users.noreply.github.com>
2023-09-19 07:23:45 -0400

    Add 01-variables.php

---
+++ php-syllabus/exercises/basics/01-variables.php
@@ -0,0 +1,21 @@
+<?php
+
+$int = 10;
+$float = 10.10;
+$string = "Hello World";
+
+echo "Exercise 1: \n";
+echo "Int: $int, float: $float, string: $string \n";
+echo "===============================\n";
+
+echo "Exercise 2: \n";
+var_dump($int,$float,$string);
+echo "===============================\n";
+
+$name = "Daniels";
+$surname = "Mongins";
+$age = 19;
+
+echo "Exercise 3: \n";
+echo $name . ' ' .$surname . ' ' .$age ."\n";
+echo "===============================\n";
